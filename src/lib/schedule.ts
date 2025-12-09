4a6e3d8123582b47f9a629ae0bc2652131958f97
Farel Abid Yasser Prayanto <123210076@student.upnyk.ac.id>
2025-12-09 13:15:54 +0700

    ganti hatma dengan salsa

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -177,7 +177,7 @@ function buildTuesday(isoDate: string): Seg[] {
     },
     { 
       start: "13:00", end: "16:00", 
-      show: "Coffee Break", host: "Abi & Hatma", 
+      show: "Coffee Break", host: "Abi & Salsa",  // Ganti Hatma dengan Salsa
       image: "/shows/coffee-break2.jpg", live: true 
     },
     { 
