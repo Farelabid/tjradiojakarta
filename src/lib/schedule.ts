ffcb81ac6271e251d6bf17ccccc7f12d5276e6e2
Farel Abid Yasser Prayanto <123210076@student.upnyk.ac.id>
2025-12-26 10:06:53 +0700

    ganti jadwal jumat

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -274,8 +274,8 @@ function buildFriday(isoDate: string): Seg[] {
     },
     { 
       start: "10:00", end: "13:00", 
-      show: "Office Hour", host: "Luvi", 
-      image: "/shows/officehour-luvi.jpg", live: true 
+      show: "Office Hour", host: "Rio", //Luvi
+      image: "/shows/officehour-rio.jpg", live: true 
     },
     { 
       start: "13:00", end: "16:00", 
@@ -284,8 +284,8 @@ function buildFriday(isoDate: string): Seg[] {
     },
     { 
       start: "16:00", end: "20:00", 
-      show: "Drive Time", host: dt.host, 
-      image: dt.image, live: true 
+      show: "Drive Time", host: "Yaser & Luvi", //dt.host
+      image: "/shows/drivetime-weekend-3.jpg", live: true //dt.image
     },
     { 
       start: "20:00", end: "24:00", 
