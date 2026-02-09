dbd502d268f2cdba3cf64e773bf3a7a4a5e9145e
Farel Abid Yasser Prayanto <123210076@student.upnyk.ac.id>
2026-02-09 10:20:44 +0700

    senin risan

--- src/lib/schedule.ts
+++ src/lib/schedule.ts
@@ -71,7 +71,7 @@ function buildMonday(_isoDate: string): Seg[] {
     { 
       start: "10:00", end: "13:00", 
       show: "Office Hour", host: "Risan", //Rio
-      image: "/shows/officehour-odah.jpg", live: true 
+      image: "/shows/officehour-risan.jpg", live: true 
     },
     { 
       start: "13:00", end: "16:00", 
